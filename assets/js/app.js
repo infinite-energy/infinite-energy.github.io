c6dfa9fbdd1609dc0232c0f3316d3888a7823cb3
Vlad George Ardelean <vlad.george.ardelean@gmail.com>
2023-05-18 22:34:02 +0300

    Add supporters page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
-//
+/**
+ * For quotes, add the "show more" option
+ */
+document.addEventListener('DOMContentLoaded', function() {
+  var showMoreLinks = document.querySelectorAll('.quote-widget .show-more');
+
+  showMoreLinks.forEach(function(link) {
+    link.addEventListener('click', function(e) {
+      e.preventDefault();
+      this.parentElement.classList.toggle('expanded');
+    });
+  });
+});
---
+++ supporters.md
@@ -0,0 +1,9 @@
+---
+layout: page
+title: Supporters
+
+sponsors: supporters
+show_sidebar: false
+---
+
+[//]: # ([View the sponsors docs]&#40;/bulma-clean-theme/docs/sponsors/&#41;)
